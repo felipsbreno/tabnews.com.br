17269c6515aed17579426e541a7cebf4e90ac9ce
felipsbreno <brendo.filipe2050@gmail.com>
2022-11-20 23:58:30 -0300

    feat(share-post): remove share and add action to copy post

--- pages/interface/components/SharePost/index.js
+++ pages/interface/components/SharePost/index.js
@@ -1,17 +1,20 @@
-import { Box } from '@primer/react';
-import { TwitterShareButton, TwitterIcon, LinkedinShareButton, LinkedinIcon } from 'react-share';
+import { Box, Button } from '@primer/react';
+import { FiCopy } from 'react-icons/fi';
 
 export default function SharePost({ objectContent }) {
+  const msgToCopy = `${objectContent.title}. Essa notícia saiu no TabNews (e você ganha TabCoins comentando lá): https://www.tabnews.com.br/${objectContent.owner_username}/${objectContent.slug}`;
+
+  function copyPost() {
+    return navigator.clipboard.writeText(msgToCopy);
+  }
+
   return (
     <Box>
-      <TwitterShareButton
-        title={`${objectContent.title}. Essa notícia saiu no TabNews (e você ganha TabCoins comentando lá):`}
-        url={`https://www.tabnews.com.br/${objectContent.owner_username}/${objectContent.slug}`}>
-        <TwitterIcon size={32} round />
-      </TwitterShareButton>
-      <LinkedinShareButton url={`https://www.tabnews.com.br/${objectContent.owner_username}/${objectContent.slug}`}>
-        <LinkedinIcon size={32} round />
-      </LinkedinShareButton>
+      {objectContent.title !== null && (
+        <Button leadingIcon={FiCopy} size="small" onClick={copyPost}>
+          Copiar post
+        </Button>
+      )}
     </Box>
   );
 }
